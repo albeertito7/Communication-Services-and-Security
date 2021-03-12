573efeea63a2da607120c2c9c47cd947efd49a71
albeertito7 <albertperezdatsira@gmail.com>
2021-03-12 14:46:17 +0100

    argument parser done

--- script.py
+++ script.py
@@ -1,20 +1,32 @@
 import sys
-import os
+import argparse, os
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('file', type=lambda x: is_file(parser, x), help='File name containing the list of triplets to be scheduled.')
+    parser.add_argument('flows', type=str, help='Fraction of the bandwidth assigned to each flow (as a percentage). Comma separated. As an example: 50,10,40')
+    parser.add_argument('outFile', type=str, nargs="?", default="outWFQ", help='Output file name containing the results')
+    parser.add_argument('-d', '--display', action='store_true', required=False, help='Display results on the screen')
+
+    return parser.parse_intermixed_args()
+
+def is_file(parser, file):
+    if not os.path.isfile(file):
+        parser_error("File path '{}' does not exist. Exiting...".format(file))
+    else:
+        return open(file, 'r')
 
 def main():
-    file = sys.argv[1]
-    flows = list(map(int, sys.argv[0].split(",")))
+    args = parse_arguments()
 
-    if not os.path.isfile(file):
-       print("File path {} does not exist. Exiting...".format(file))
-       sys.exit(-1)
-       
-    list = []
-    with open(file) as reader: [list.append(tuple(line.strip().split(" "))) for line in reader]
+    data = list()
+    [data.append(tuple(line.strip().split(" "))) for line in args.file.readlines()]
 
-    print(list)
+    flows = list(map(int, args.flows.split(",")))
+
+    print(data)
     print(flows)
-
 
 if __name__ == '__main__':
     main()
