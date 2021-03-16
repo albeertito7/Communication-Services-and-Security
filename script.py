7a95596006fc53df1a1e928a1ccd67e1eb1dace4
albeertito7 <albertperezdatsira@gmail.com>
2021-03-16 13:35:31 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,14 @@
 import sys
 import argparse, os
+from functools import partial
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('file', type=lambda x: is_file(parser, x), help='File name containing the list of triplets to be scheduled.')
     parser.add_argument('flows', type=str, help='Fraction of the bandwidth assigned to each flow (as a percentage). Comma separated. As an example: 50,10,40')
-    parser.add_argument('outFile', type=str, nargs="?", default="outWFQ", help='Output file name containing the results')
-    # parser.add_argument('-d', '--display', action='store_true', required=False, help='Display results on the screen')
+    #parser.add_argument('outFile', type=str, required=False, help='Output file name containing the results')
+    #parser.add_argument('-d', '--display', action='store_true', required=False, help='Display results on the screen')
 
     return parser.parse_intermixed_args()
 
@@ -21,37 +22,39 @@ def main():
     args = parse_arguments()
 
     data = [list(map(float, line.strip().split())) for line in args.file.readlines()]
-    flows = [x/100 for x in list(map(int, args.flows.split(",")))]
-
-    print("Flows: ", flows)
+    flows = [x/100 for x in list(map(float, args.flows.split(",")))]
 
     result = schedule(data, flows, type="WFQ")
     printResult(result)
 
-def schedule(data, flows, type):
-    return {
-        "FQ": fair_queueing(data),
-        "WFQ": weighted_fair_queueing(data, flows)
-    }.get(type, "Error: scheduler mechanism not specified")
+def schedule(data, flows=None, type="FQ"):
+    switcher = {
+        "FQ": partial(fair_queueing, data),
+        "WFQ": partial(weighted_fair_queueing, data, flows)
+    }
+    return switcher.get(type, errorHandler)()
     
+def errorHandler():
+    return "Error: scheduler mechanism not specified"
+
 def fair_queueing(data, timeFinish=0, packets_received=[], packets_delivered=[]):
-        
+    
     if len(data) <= 0 and len(packets_received) <= 0:
         return packets_delivered
 
     count_adds = 0
 
     for packet in data:
-        if packet[0] <= timeFinish:
-            time_estimated = max(timeFinish, packet[0]) + packet[1]
+        if packet[1] <= timeFinish:
+            time_estimated = max(timeFinish, packet[1]) + packet[2]
             packets_received.append(packet+[time_estimated]) # packets received at a time
             count_adds += 1
 
     if(len(packets_received) <= 0 and count_adds == 0):
-        timeFinish = data[0][0]
+        timeFinish = data[0][1]
         for packet in data:
-            if packet[0] <= timeFinish:
-                time_estimated = max(timeFinish, packet[0]) + packet[1]
+            if packet[1] <= timeFinish:
+                time_estimated = max(timeFinish, packet[1]) + packet[2]
                 packets_received.append(packet+[time_estimated]) # packets received at a time
                 count_adds += 1
 
@@ -59,11 +62,11 @@ def fair_queueing(data, timeFinish=0, packets_received=[], packets_delivered=[])
     print("Data to receive: ", data)
     print("Received packets: ", packets_received)
 
-    tmp = packets_received[0][3]
+    tmp = packets_received[0][4]
     packet_less = packets_received[0]
     for less in packets_received:
-        if less[3] < tmp:
-            tmp = less[3]
+        if less[4] < tmp:
+            tmp = less[4]
             packet_less = less
 
     timeFinish+=packet_less[1] # size sum
@@ -78,22 +81,24 @@ def fair_queueing(data, timeFinish=0, packets_received=[], packets_delivered=[])
 
 
 def weighted_fair_queueing(data, flows, timeFinish=0, packets_received=[], packets_delivered=[]):
+
     if len(data) <= 0 and len(packets_received) <= 0:
         return packets_delivered
 
     count_adds = 0
 
     for packet in data:
-        if packet[0] <= timeFinish:
-            time_estimated = max(timeFinish, packet[0]) + packet[1]*flows[int(packet[2])-1]
+        if packet[1] <= timeFinish:
+            time_estimated = max(timeFinish, packet[1]) + packet[2]*flows[int(packet[3])-1]
+            print(flows[int(packet[3])-1], time_estimated)
             packets_received.append(packet+[time_estimated]) # packets received at a time
             count_adds += 1
 
     if(len(packets_received) <= 0 and count_adds == 0):
-        timeFinish = data[0][0]
+        timeFinish = data[0][1]
         for packet in data:
-            if packet[0] <= timeFinish:
-                time_estimated = max(timeFinish, packet[0]) + packet[1]*flows[int(packet[2])-1]
+            if packet[1] <= timeFinish:
+                time_estimated = max(timeFinish, packet[1]) + packet[2]*flows[int(packet[3])-1]
                 packets_received.append(packet+[time_estimated]) # packets received at a time
                 count_adds += 1
 
@@ -101,14 +106,14 @@ def weighted_fair_queueing(data, flows, timeFinish=0, packets_received=[], packe
     print("Data to receive: ", data)
     print("Received packets: ", packets_received)
 
-    tmp = packets_received[0][3]
+    tmp = packets_received[0][4]
     packet_less = packets_received[0]
     for less in packets_received:
-        if less[3] < tmp:
-            tmp = less[3]
+        if less[4] < tmp:
+            tmp = less[4]
             packet_less = less
 
-    timeFinish+=packet_less[1] # size sum
+    timeFinish+=packet_less[2] # size sum
 
     packets_received.remove(packet_less) # remove first occurrence
     packets_delivered.append(packet_less+[timeFinish]) # packet with less time sent
@@ -118,8 +123,9 @@ def weighted_fair_queueing(data, flows, timeFinish=0, packets_received=[], packe
 
     return weighted_fair_queueing(data, flows, timeFinish, packets_received, packets_delivered)
 
-def printResult(data):
-    pass
+def printResult(data): 
+    print("Result: ", [x[0] for x in data])
+
 
 if __name__ == '__main__':
     main()
