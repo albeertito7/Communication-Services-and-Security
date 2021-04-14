8e5e2263b62eb2399fb49de0c7622d28224bf686
albeertito7 <albertperezdatsira@gmail.com>
2021-04-14 20:15:00 +0200

    Update test.sh

--- exercise3/Problem1/test.sh
+++ exercise3/Problem1/test.sh
@@ -21,34 +21,42 @@ echo "Processing $file file ..."
 # frame (wireshark frame number)
 # -Y | --display-filter <displaY filter> (Packets matching the filter are printed or written to file)
 
+get_bytes()
+{
+    bytes_arraw=$1
+    bytes=0
+    for x in ${bytes_array[*]}; do
+	    bytes=$[ $bytes + $x ];
+    done
+    echo $bytes
+}
+
+process()
+{
+    echo ""
+    echo "$1: $2 Bytes"
+    avg_rate=$(bc <<< "$2/$time")
+    echo "Avg rate: $avg_rate Bps"
+    echo ""
+}
+
 time=$(tshark -Y "ip.dsfield.dscp == 56  and ip.src == 11.0.0.1" -r captureP1.pcapng -T fields -e frame.time_relative | tail -1)
 
 bytes_array=$(tshark -Y "ip.dsfield.dscp == 56  and ip.src == 11.0.0.1" -r captureP1.pcapng -T fields -e frame.len)
-bytes=0
-for x in ${bytes_array[*]}; do
-	bytes=$[ $bytes + $x ];
-done
-
-echo ""
-echo "Tap0 Precedence 7: $bytes Bytes"
-#bits=$[$bytes*8]
-avg_rate=$(bc <<< "$bytes/$time")
-echo "Avg rate: $average_rate Bps"
-echo ""
-
+bytes=$(get_bytes $bytes_array)
+process "Tap0 IP Precedence 7" $bytes
 total_bytes=$bytes
 
 bytes_array=$(tshark -Y "ip.dsfield.dscp == 8  and ip.src == 11.0.0.1" -r captureP1.pcapng -T fields -e frame.len)
-bytes=0
-for x in ${bytes_array[*]}; do
-	bytes=$[ $bytes + $x ];
-done
-
-echo ""
-echo "Tap0 Precedence 1: $bytes Bytes"
-#bits=$[$bytes*8]
-avg_rate=$(bc <<< "$bytes/$time")
-echo "Avg rate: $average_rate Bps"
-echo ""
-
+bytes=$(get_bytes $bytes_array)
+process "Tap0 IP Precedence 1" $bytes
 total_bytes=[$total_bytes + $bytes]
+
+bytes_array=$(tshark -Y "ip.src == 12.0.0.1" -r captureP1.pcapng -T fields -e frame.len)
+bytes=$(get_bytes $bytes_array)
+process "Tap1 IP Without Precedence" $bytes
+total_bytes=[$total_bytes + $bytes]
+
+echo ""
+echo "Total bytes: $total_bytes"
+echo ""
