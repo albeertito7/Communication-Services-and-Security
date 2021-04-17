aa306d601886fc2e600c135f1bbc6fd638e5a69f
albeertito7 <albertperezdatsira@gmail.com>
2021-04-17 17:24:39 +0200

    Update script.sh

--- exercise3/problem2/script.sh
+++ exercise3/problem2/script.sh
@@ -5,9 +5,9 @@ display_usage()
     echo -e "\nUsage: $0 [options] <infile>"
     echo -e "\nShell script that computes from the capture frame infile the percentatge of bandwidth ocuppation for each of the streams coming from C1-tap0 and C2-tap1."
     echo -e "\nrequired arguments:"
-    echo -e "   infile:         Must be a .pcap or .pcapng file"
+    echo "   infile:         Must be a .pcap or .pcapng file"
     echo -e "\noptional arguments:"
-    echo -e "   -h, --help      Shows usage message to provide help"
+    echo "   -h, --help      Shows usage message to provide help"
 } 
 
 file=$1
@@ -40,7 +40,6 @@ get_bytes()
     echo $bytes
 }
 
-
 bytes_array=$(tshark -Y "ip.src == 11.0.0.1" -r $file -T fields -e frame.len)
 tap0_bytes=$(get_bytes $bytes_array)
 total_bytes=$tap0_bytes
@@ -49,15 +48,13 @@ bytes_array=$(tshark -Y "ip.src == 12.0.0.1" -r $file -T fields -e frame.len)
 tap1_bytes=$(get_bytes $bytes_array)
 total_bytes=$[$tap1_bytes + $total_bytes]
 
-echo ""
-echo "Total bytes transfered: $total_bytes Bytes"
 bandwith_occupation1=$(bc <<< "($tap0_bytes*100.0)/$total_bytes")
-echo ""
-echo "TAP 0: $bytes1 Bytes"
+echo -e "\nTAP 0: $bytes1 Bytes"
 echo "Bandwidth occupation TAP 0: $bandwith_occupation1%"
-echo ""
+
 bandwith_occupation=$(bc <<< "($tap1_bytes*100.0)/$total_bytes")
-echo "TAP 1: $bytes Bytes"
+echo -e "\nTAP 1: $bytes Bytes"
 echo "Bandwidth occupation TAP 1: $bandwith_occupation%"
-echo ""
+
+echo -e "\nTotal bytes: $total_bytes Bytes\n"
 
