c9acc4574887dafc04d5fea73a808114c55b40fc
albeertito7 <albertperezdatsira@gmail.com>
2021-05-01 23:32:33 +0200

    last changes added

--- exercise4/problem1/part2/counter.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if __name__ == '__main__':
-    results = 0
-    with open(sys.argv[1], 'r') as file:
-        for line in file.readlines():
-            if line.__contains__('RTP'):
-                results += int(line.split(' ')[5])
-        print('Results: %s' % results)
